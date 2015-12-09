62165af63ace34f98f95fab032fc5f8a9ad15c3d
Vladislav <v.kirbaba@gmail.com>
2015-12-09 14:52:10 +0300

    Новости

--- js/script.js
+++ js/script.js
@@ -1,35 +1,3 @@
-
-/*-------------GOOGLE MAPS-----------------*/
-
-/*function initialize() {
-
-    var myLatlng = new google.maps.LatLng(59.934602, 30.334607);
-    var mapOptions = {
-        center: new google.maps.LatLng(59.934602, 30.334607),
-        zoom: 17,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        scrollwheel: false
-    };
-    var map = new google.maps.Map(document.getElementById("map_canvas"),
-        mapOptions);
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        title:"Ditlogistic"
-    });
-}
-
-function loadScript() {
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyAaOWKyamSxMTXclSDFmJ2N4Am20PCTD6I&sensor=FALSE&callback=initialize";
-    document.body.appendChild(script);
-}
-
-window.onload = loadScript;
-*/
-
-
 $(function() {
 
     $(window).scroll(function() {
@@ -54,3 +22,24 @@ $(function() {
     });
 });
 
+jQuery(function ($) {
+    $('.hamburger-slim').on('click', function (e) {
+        e.preventDefault();
+        $(this).toggleClass('hamburger-slim--active');
+    });
+});
+
+jQuery(function ($) {
+
+    jQuery('.news-carousel').slick({
+        dots: false,
+        infinite: true,
+        autoplay: false,
+        speed: 300,
+        focusOnSelect: false,
+        arrows: true,        
+        slidesToShow: 1,
+        slidesToScroll: 1
+        
+    });
+});
