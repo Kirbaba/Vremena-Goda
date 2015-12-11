1943a2ba7eba398caf26e33eff3363fcab488f5d
Vladislav <v.kirbaba@gmail.com>
2015-12-11 16:20:52 +0300

    Новости

--- js/script.js
+++ js/script.js
@@ -151,3 +151,36 @@ function init() {
         Map.geoObjects.add(Placemark);
 }
 }
+
+$(document).ready(function () {
+    $("[data-js=open]").on("click", function() {
+        popupOpenClose($(".popup"));
+    });
+});
+
+jQuery(function($){
+    $.datepicker.regional['ru'] = {
+        closeText: 'Закрыть',
+        prevText: '&#x3C;Пред',
+        nextText: 'След&#x3E;',
+        currentText: 'Сегодня',
+        monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+       'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+        monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
+        'Июл','Авг','Сен','Окт','Ноя','Дек'],
+        dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+        dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+        dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+        weekHeader: 'Нед',
+        dateFormat: 'dd.mm.yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''};
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
+});
+
+$(function() { 
+    $( "#datepicker" ).datepicker(); 
+  
+}); 
